9e6bb328632f091ef5d37c8a5ac908e5d283d2bf
pravind002 <parvindraj004@gmail.com>
2025-07-25 16:39:13 +0530

    init

---
+++ lib/main.dart
@@ -0,0 +1,62 @@
+import 'package:flutter/material.dart';
+import 'package:webview_flutter/webview_flutter.dart';
+
+void main() {
+  runApp(const MyWebApp());
+}
+
+class MyWebApp extends StatelessWidget {
+  const MyWebApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'WebView App',
+      debugShowCheckedModeBanner: false,
+      home: const MyWebViewPage(),
+    );
+  }
+}
+
+class MyWebViewPage extends StatefulWidget {
+  const MyWebViewPage({super.key});
+
+  @override
+  State<MyWebViewPage> createState() => _MyWebViewPageState();
+}
+
+class _MyWebViewPageState extends State<MyWebViewPage> {
+  late final WebViewController _controller;
+  bool isLoading = true;
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = WebViewController()
+      ..setJavaScriptMode(JavaScriptMode.unrestricted)
+      ..setNavigationDelegate(
+        NavigationDelegate(
+          onPageFinished: (_) {
+            setState(() => isLoading = false);
+          },
+        ),
+      )
+      ..loadRequest(
+        Uri.parse('https://writelys.vercel.app/'),
+      ); // 🔁 change URL here
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: Scaffold(
+        body: Stack(
+          children: [
+            WebViewWidget(controller: _controller),
+            if (isLoading) const Center(child: CircularProgressIndicator()),
+          ],
+        ),
+      ),
+    );
+  }
+}
