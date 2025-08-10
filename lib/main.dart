7182d47f4c8cff55f47244238f6f285b2e769aeb
pravind002 <parvindraj004@gmail.com>
2025-08-10 17:27:49 +0530

    change logo and add internet permission

--- lib/main.dart
+++ lib/main.dart
@@ -11,7 +11,7 @@ class MyWebApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'WebView App',
+      title: 'Writely',
       debugShowCheckedModeBanner: false,
       home: const MyWebViewPage(),
     );
@@ -60,3 +60,82 @@ class _MyWebViewPageState extends State<MyWebViewPage> {
     );
   }
 }
+
+// import 'package:flutter/material.dart';
+// import 'package:webview_flutter/webview_flutter.dart';
+
+// void main() {
+//   runApp(const MyWebApp());
+// }
+
+// class MyWebApp extends StatelessWidget {
+//   const MyWebApp({super.key});
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return MaterialApp(
+//       title: 'WebView App',
+//       debugShowCheckedModeBanner: false,
+//       home: const MyWebViewPage(),
+//     );
+//   }
+// }
+
+// class MyWebViewPage extends StatefulWidget {
+//   const MyWebViewPage({super.key});
+
+//   @override
+//   State<MyWebViewPage> createState() => _MyWebViewPageState();
+// }
+
+// class _MyWebViewPageState extends State<MyWebViewPage> {
+//   late final WebViewController _controller;
+//   bool isLoading = true;
+//   String? errorMessage;
+
+//   @override
+//   void initState() {
+//     super.initState();
+
+//     _controller = WebViewController()
+//       ..setJavaScriptMode(JavaScriptMode.unrestricted)
+//       ..setNavigationDelegate(
+//         NavigationDelegate(
+//           onPageFinished: (_) {
+//             setState(() => isLoading = false);
+//           },
+//           onWebResourceError: (WebResourceError error) {
+//             setState(() {
+//               isLoading = false;
+//               errorMessage =
+//                   "Error: ${error.description}\nCode: ${error.errorCode}";
+//             });
+//           },
+//         ),
+//       )
+//       ..loadRequest(Uri.parse('https://writelys.vercel.app/'));
+//   }
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return SafeArea(
+//       child: Scaffold(
+//         body: Stack(
+//           children: [
+//             if (errorMessage == null)
+//               WebViewWidget(controller: _controller)
+//             else
+//               Center(
+//                 child: Text(
+//                   errorMessage!,
+//                   style: const TextStyle(color: Colors.red),
+//                   textAlign: TextAlign.center,
+//                 ),
+//               ),
+//             if (isLoading) const Center(child: CircularProgressIndicator()),
+//           ],
+//         ),
+//       ),
+//     );
+//   }
+// }
